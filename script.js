0358d41e7eba19076ce3f42630034b2ba49a6f45
JohnjiRomanji <etashjhanji@gmail.com>
2023-08-15 15:17:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -218,8 +218,8 @@ function generateRandomString(length) {
 
   
 const clientId = 'a517b42be802471ea6d45a616bb09845';
-const redirectUri = 'http://127.0.0.1:5500';
-//const redirectUri = 'https://card-ify.netlify.app/';
+//const redirectUri = 'http://127.0.0.1:5500';
+const redirectUri = 'https://card-ify.netlify.app/';
 function loginSpotify() {
     generateCodeVerifierAndChallenge(128).then(({ codeVerifier, codeChallenge }) => {
         console.log('Code Verifier:', codeVerifier);
